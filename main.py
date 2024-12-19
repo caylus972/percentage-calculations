cf2e65bd0622e10fc5f3f6031d68ead170e61a5a
caylus972 <pamigeiadigwe@gmail.com>
2024-12-19 20:02:18 +0000

    third comit

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ print("enter marks obtained in 4 subjects:")
 math= int(input("maths :"))
 english = int(input("english :"))
 science = int(input("science :"))
-gps =int(input("gps :"))
+PE =int(input("PE :"))
 
 # let calculate the percentage of marks
-sum = math+english+science+gps
-print("sum of math,english,science and gps")
+sum = math+english+science+PE
+print("sum of math,english,science and PE")
 
 perc = (sum/400)*100
 
